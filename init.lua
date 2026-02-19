99831732712c9fd998467256c810db4317a76ffb
Alex Mellor <alexander.mellor@hey.com>
2026-02-19 15:12:05 +0000

    Remove rubocop and ruby_lsp

--- init.lua
+++ init.lua
@@ -685,8 +685,8 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        ruby_lsp = {},
-        rubocop = {},
+        -- ruby_lsp = {},
+        -- rubocop = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
